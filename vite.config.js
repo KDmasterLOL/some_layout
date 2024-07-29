222c6a1f86fa363e7c9d4c2dabd9120cd9333c31
KDmasterLOL <ivantolmachevmaster@gmail.com>
2024-07-29 17:37:57 +0300

    Some changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: 'src',
   publicDir: '../public/',
+  base: '/some_layout/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
